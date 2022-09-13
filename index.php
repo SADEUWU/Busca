4e6640a4be3e75d6c15fa7a50ac69b55c9a866e1
unknown <duduluis955@gmail.com>
2022-09-13 00:09:28 -0300

    First commit

---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>Busca PHP</title>
+</head>
+<body>
+	<h2>Pesquisa de Livros</h2>
+	<form action="busca.php" method="GET">
+		<label>Nome do livro</labe>
+		<input type="text" name="nome_livro" size="50" placeholder="Incira o nome do livro">
+		<button style="width:100px;">Buscar</button>
+	</form>
+
+</body>
+</html>
+
